c75bb09bbd674163a51cfe36e5d29159bd5aff8f
Joshua Gelberger <joshua_gelberger@alumni.brown.edu>
2018-08-29 19:46:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function takeANumber(currentLine, newPerson) {
+  var spot = currentLine.length + 1;
+  currentLine.push(newPerson);
+  return `Welcome, ${newPerson}. You are number ${spot} in line.`;
+}
+
+function nowServing(currentLine) {
+  if (currentLine.length > 0) {
+    var served = currentLine[0];
+    currentLine.shift();
+    return `Currently serving ${served}.`;
+  } else {
+    return 'There is nobody waiting to be served!';
+  }
+}
+
+function currentLine(currentLine) {
+  if (currentLine.length > 0) {
+    var line = {};
+    while (currentLine.length > 0) {
+      
+      line[key] = currentLine[0];
+      currentLine.shift();
+    }
+  } else {
+    return 'The line is currently empty.';
+  }
+}
